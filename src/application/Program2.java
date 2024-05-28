b07282d6d70e9cede394738f579ebb8e23d030e1
FFutagawa <ffutagawa@gmail.com>
2024-05-27 22:36:23 -0300

    Implementation of findById an findAll of the department class

---
+++ src/application/Program2.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.List;
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TEST 1: findById =======");
+		System.out.print("Informe um departamento para pesquisar: ");
+		int id = sc.nextInt();
+		Department dep = departmentDao.findById(id);
+		System.out.println(dep);
+
+		System.out.println("\n=== TEST 2: findAll =======");
+		List<Department> list = departmentDao.findAll();
+		for (Department d : list) {
+			System.out.println(d);
+		}
+		
+	}
+
+}
